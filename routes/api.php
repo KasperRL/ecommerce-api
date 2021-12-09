cbf56fef3279b95cabc6c852b766ec9df08edeb2
Kasper <kasper.ligthart@icloud.com>
2021-12-09 22:24:01 +0100

    Update: update route is now PUT instead of POST

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::post('/products', [ProductController::class, 'store']);
-// TODO: POST -> PUT/PATCH
-Route::post('/products/{id}', [ProductController::class, 'update']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
 Route::delete('/products/{id}', [ProductController::class, 'destroy']);
